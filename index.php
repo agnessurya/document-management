83ad7f5803c34f0baf10590343c87cd8a5f90aa5
Agnes Suryani <el.agnesurya@gmail.com>
2022-02-21 04:48:55 +0700

    delete update

--- index.php
+++ index.php
@@ -4,30 +4,80 @@ $user = "root";
 $pass = "";
 $db = "document";
 
+
 $connection = mysqli_connect($host,$user,$pass,$db);
 if (!$connection) {
     die("Could not connect to the database");
 }
 
 $title = "";
+$file = "";
 $error = "";
 $success = "";
 
-if (isset($_POST['submit'])) {
+if (isset($_GET['op'])){
+    $op = $_GET['op'];
+}else{
+    $op = "";
+}
+if($op === 'edit'){
+    $id = $_GET['id'];
+    $sql3 = "select * from documents where id = '$id'";
+    $q3 = mysqli_query($connection,$sql3);
+    $r1 = mysqli_fetch_array($q3);
+    if($r1){
+        $id = $r1['id'];
+        $title = $r1['title'];
+    }
+
+    if ($title == "") {
+        $error = "Document Not Found";
+    }
+}
+
+if ($op === 'delete'){
+    $id = $_GET['id'];
+    $sql5 = "delete from documents where id = '$id'";
+    $q5 = mysqli_query($connection,$sql5);
+
+    if ($q5) {
+        $success = "Success Delete Document";
+    }else{
+        $error = "Failed Delete Document";
+    }
+} 
+
+if (isset($_POST['submit'])) {// add document
    $title = $_POST['title'];
+   $file = $_FILES['file'];
 
    if ($title) {
-      $sql1 = "insert into documents (title) values('$title')";
-      $q1 = mysqli_query($connection,$sql1);
+    if ($op === 'edit') {
+        $sql4 = "update documents set title = '$title' where id = '$id'";
+        $q4 = mysqli_query($connection,$sql4);
 
-        if ($q1) {
-            $success = "Success Add New Document";
-        }else {
-            $error = "Failed Add New Document";
-        }  
+        if ($q4) {
+              $success = "Success Updating Document";
+              header("location: index.php");
+        }else{
+            $error = "Failed Updating Data";
+        }
+    }else{
+        $sql1 = "insert into documents (title) values('$title')";
+        $q1 = mysqli_query($connection,$sql1);
+  
+          if ($q1) {
+              $success = "Success Add New Document";
+              header("location: index.php");
+          }else{
+              $error = "Failed Add New Document";
+          }  
+    }
     }else{
         $error = "Please Input All Data";
     }
+
+   
 }   
 
 ?>
@@ -41,7 +91,7 @@ if (isset($_POST['submit'])) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <style>
         .mx-auto{
-            width: 80%;
+            width: 70%;
         }
         .card{
             margin: 10px
@@ -75,11 +125,17 @@ if (isset($_POST['submit'])) {
                 <?php
                     }
                 ?>
-                <form action="" method="POST">
+                <form action="" method="POST" enctype="multipart/form-data">
                     <div class="mb-3 row">
                             <label for="name" class="col-sm-2 col-form-label">Title </label>
                         <div class="col-sm-10">
                             <input type="text" class="form-control" id="title" name="title" placeholder="Document Title" value="<?php echo $title ?>">
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                            <label for="name" class="col-sm-2 col-form-label">File </label>
+                        <div class="col-sm-10">
+                            <input type="file" class="form-control" id="title" name="file" placeholder="Document Title" value="<?php echo $file ?>">
                         </div>
                     </div>
                     <div class="col-12">
@@ -110,14 +166,13 @@ if (isset($_POST['submit'])) {
                             while($r2 = mysqli_fetch_array($q2)){
                                 $id = $r2['id'];
                                 $title = $r2['title'];
-                         
                             ?>
                             <tr>
                                <td scope="row"><?php echo $index++  ?></th> 
                                <td scope='row'><?php echo $title  ?></td>
                                <td>
-                                    <button type="button" class="btn btn-success">Edit</button>
-                                    <button type="button" class="btn btn-danger">Delete</button>
+                                   <a href="index.php?op=edit&id=<?php echo $id ?>"><button type="button" class="btn btn-success">Edit</button></a>
+                                   <a href="index.php?op=delete&id=<?php echo $id ?>"><button type="button" class="btn btn-danger" onclick="return confirm('are you gonna delete this document?')">Delete</button></a> 
                                </td>
                             </tr>
                         <?php
